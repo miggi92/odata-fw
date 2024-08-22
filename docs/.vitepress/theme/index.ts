d683c1a0966ad0414148b761f3ab37bfe0b99763
Miguel Gebhardt <miguel.gebhardt@bechtle.com>
2024-08-22 03:47:51 +0200

    feat: :sparkles: added zoom to pictures

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,22 @@
 import DefaultTheme from 'vitepress/theme';
 import Mermaid from './Mermaid.vue';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import type { EnhanceAppContext } from 'vitepress';
+import { useRoute } from 'vitepress';
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }: EnhanceAppContext) {
     // register global components
     app.component('Mermaid', Mermaid);
+    // Register global components, if you don't want to use it, you don't need to add it
+    app.component('vImageViewer', vImageViewer);
   },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
+  }
 };

b0ac91728404d6e3ef8a913624712e462869c310
Miguel <miggi92@users.noreply.github.com>
2026-02-26 17:09:11 +0100

    chore: added mcp from template and updated server routes

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -6,7 +6,8 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/content',
     'nuxt-og-image',
-    'nuxt-llms'
+    'nuxt-llms',
+    '@nuxtjs/mcp-toolkit'
   ],
 
   devtools: {
@@ -31,6 +32,9 @@ export default defineNuxtConfig({
         }
       }
     }
+  },
+  experimental: {
+    asyncContext: true
   },
 
   compatibilityDate: '2024-07-11',
@@ -75,5 +79,8 @@ export default defineNuxtConfig({
         ]
       }
     ]
+  },
+  mcp: {
+    name: 'Docs template'
   }
 })
